9e3a9aa83249807821e5f1ab9b015f70da4fc54c
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2023-11-27 21:40:48 +0300

    completed

--- MVCProject/Controllers/HomeController.cs
+++ MVCProject/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCProject.Models.ApiModel;
 
 namespace MVCProject.Controllers
 {
     public class HomeController : Controller
     {
-  
-
-        public IActionResult Index()
+        // TPL
+        public async Task<IActionResult> Index()
         {
+            // HttpClientFactory,
+            //  MULTITASKING MULTITHREADING
+
+            // Veritabanı işlemlerinde, dosya işlemlerinde, s2s call, loglama,
+            
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:5246/");
+           var responseMessage =  await client.GetAsync("api/Products");
+
+
+            //if (responseMessage.IsSuccessStatusCode)
+            //{
+            //    var data = await responseMessage.Content.ReadAsStringAsync();
+            //}
+
             return View();
+
+
         }
 
         public IActionResult Privacy()
---
+++ MVCProject/Models/ApiModel/ProductResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace MVCProject.Models.ApiModel
+{
+    public class ProductResponse
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
